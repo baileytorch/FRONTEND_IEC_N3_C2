cb9084a468c82ecd3c15e47d69907eb97df38e50
baileytorch <baileytorch@hotmail.com>
2026-06-05 13:06:01 -0400

    Validando formulario

---
+++ js/procesar_form.js
@@ -0,0 +1,25 @@
+function validarFormulario(){
+    let campoNombre = document.getElementById('input_nombre');
+    let campoRut = document.getElementById('input_rut');
+    let campoEmail = document.getElementById('input_email');
+    let campoFechaNacimiento = document.getElementById('inputFechaNac');
+    let redioGenero = document.querySelector('input[name="radioGenero"]:checked');
+    let campoContrasena = document.getElementById('input_contrasena');
+    let campoRepetirContrasena = document.getElementById('input_confirm_contrasena');
+
+    if(campoNombre.value == ''){
+        alert('Debe Ingresar el Nombre!')
+    }else{}
+}
+
+function funcionPrueba(){
+    alert('Click!')
+}
+
+function funcionFocus(){
+    alert('Focus en el elemento')
+}
+
+function funcionBlur(){
+    alert('Perdimos el focus del elemento')
+}
