cdbd4d5a12610a2fd941486a1f0518228961fb1c
baileytorch <baileytorch@hotmail.com>
2026-06-26 13:51:10 -0400

    Validando con alertas

--- js/procesar_form.js
+++ js/procesar_form.js
@@ -1,3 +1,13 @@
+window.onload = function(){
+    document.getElementById('alertaNombre').style.display = 'none';
+    document.getElementById('alertaRut').style.display = 'none';
+    document.getElementById('alertaEmail').style.display = 'none';
+    document.getElementById('alertaFechaNacimiento').style.display = 'none';
+    document.getElementById('alertaGenero').style.display = 'none';
+    document.getElementById('alertaContrasena').style.display = 'none';
+    document.getElementById('alertaRepetirContrasena').style.display = 'none';
+};
+
 function validarFormulario() {
     let campoNombre = document.getElementById('input_nombre');
     let campoRut = document.getElementById('input_rut');
@@ -8,12 +18,18 @@ function validarFormulario() {
     let campoRepetirContrasena = document.getElementById('input_confirm_contrasena');
     let formularioValido = true;
 
-    if (!validarInput(campoNombre)) { formularioValido = false; }
-    if (!validarInput(campoRut)) { formularioValido = false; }
-    if (!validarEmail(campoEmail)) { formularioValido = false; }
+    if (!validarInput(campoNombre)) { 
+        document.getElementById('alertaNombre').style.display = 'block';
+        formularioValido = false; 
+    }
+    if (!validarEmail(campoEmail)) { 
+        document.getElementById('alertaRut').style.display = 'block';
+        formularioValido = false; 
+    }
+    if (!validarRut(campoRut)) { formularioValido = false }
     if (!validarInput(campoFechaNacimiento)) { formularioValido = false; }
     if (!validarContraseñaSegura(campoContrasena)) { formularioValido = false; }
-    if (!validarRepetirContrasena(campoRepetirContrasena,campoContrasena)) { formularioValido = false; }
+    if (!validarRepetirContrasena(campoRepetirContrasena, campoContrasena)) { formularioValido = false; }
 
     if (formularioValido) {
         alert('Formulario Válido, enviando datos al servidor.')
@@ -73,3 +89,36 @@ function validarRepetirContrasena(elemento, elemento2) {
         }
     }
 }
+
+function validarRut(elemento) {
+    // Valida el rut con su cadena completa "XXXXXXXX-X"
+    regexRut = /^[0-9]+[-‐]{1}[0-9kK]{1}$/;
+    const rutCompleto = elemento.value.replaceAll('.','');
+
+    if (validarInput(elemento)) {
+        if (!regexRut.test(rutCompleto)) {
+            elemento.classList.add('inputInvalido', 'is-invalid');
+            return false
+        } else {
+            var tmp = rutCompleto.split('-');
+            var digv = tmp[1];
+            var rut = tmp[0];
+            if (digv == 'K') digv = 'k';
+            if (calculoDv(rut) == digv) {
+                elemento.classList.remove('inputInvalido', 'is-invalid');
+                elemento.classList.add('is-valid');
+                return true
+            } else {
+                elemento.classList.add('inputInvalido', 'is-invalid');
+                return false
+            };
+        }
+    }
+};
+
+function calculoDv(T) {
+    var M = 0, S = 1;
+    for (; T; T = Math.floor(T / 10))
+        S = (S + T % 10 * (9 - M++ % 6)) % 11;
+    return S ? S - 1 : 'k';
+};
